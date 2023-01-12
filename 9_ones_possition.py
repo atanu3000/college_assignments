b47fb8e2c92a2f8fb34fd5882298b2130d3db815
Atanu Paul <atanupaul2411@gmail.com>
2023-01-12 08:31:00 +0530

    code updated

---
+++ 9_ones_possition.py
@@ -0,0 +1,10 @@
+# 9. WAP to print ones possition of a number.
+
+num = int(input("Enter the number: "))
+ones = num % 10
+print("Ones possition of the number: ", ones)
+
+
+# Output:
+# Enter the number: 5234
+# Ones possition of the number:  4
