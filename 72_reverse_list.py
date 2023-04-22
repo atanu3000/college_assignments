38696b7c1f53b986b131c51e96483307e7463c21
Atanu Paul <atanupaul2411@gmail.com>
2023-04-22 11:21:08 +0530

    reverse a list

---
+++ 72_reverse_list.py
@@ -0,0 +1,22 @@
+# 72. wap to create a list of numbers in the specified range in perticular steps. reverse the list and print the values.
+
+length = int(input("Enter the length of list: "))
+
+list = [i for i in range(1, length + 1)]
+
+# # Method 1
+# list = list[::-1]
+
+# Method 2
+l = len(list)
+for i in range(l // 2):
+    list[i], list[l - 1 - i] = list[l - 1 - i], list[i]
+
+# # Method 3
+# l = len(list)
+# for i in range(l // 2):
+#     temp = list[i]
+#     list[i] = list[l - 1 - i]
+#     list[l - 1 - i] = temp
+
+print(list)
